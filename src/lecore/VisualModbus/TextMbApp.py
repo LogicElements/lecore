cd96e6999af3e150654acdbc046c714de1f64dfe
LE\jan.bartovsky <jan.bartovsky@logicelements.cz>
2023-12-03 22:59:36 +0100

    Add update screen and improve settings screen.

--- src/lecore/VisualModbus/TextMbApp.py
+++ src/lecore/VisualModbus/TextMbApp.py
@@ -1,10 +1,12 @@
 import time
+import pathlib
+
 from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Button, Header, Static, Input, Footer
-from textual.containers import Horizontal, Vertical
+from textual.widgets import Button, Header, Static, Input, Footer, DirectoryTree, ProgressBar
+from textual.containers import Horizontal, VerticalScroll
 from textual.binding import Binding
-from textual.screen import Screen
+from textual.screen import ModalScreen
 from textual.message import Message
 from textual.widgets import DataTable
 
@@ -36,45 +38,133 @@ class MyInput(Input):
         pass
 
     async def on_key(self, event):
-        self.log(event, event.key)
         if event.key == 'escape':
             self.post_message(self.Return())
         await super()._on_key(event)
 
 
-class Settings(Screen):
-    BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
+class Settings(ModalScreen):
+    BINDINGS = [
+        Binding("ctrl+s", "ftr_save", "OK"),
+        Binding("escape", "app.pop_screen", "Back"),
+    ]
+
+    KEYS = {
+        "comport": "Communication serial port name, e.g., COM1",
+        "baud_rate": "Communication baud rate at bauds per second, e.g., 19200",
+        "stop_bits": "Number of stop bit, must be one from the following: 1, 1.5, 2",
+        "parity": "Parity type, must be one from the following: 'N' for None, 'E' for even, 'O' for odd",
+        "timeout": "Timeout of slave response time in seconds",
+        "slave_address": "Slave address number",
+        "reg_map": "Name and path to register map definition json file",
+        "type_binary": "Binary type or identifier used for firmware update",
+        "init_delay": "Initial delay for flash deletion at the start of firmware update",
+    }
+
+    class Return(Message):
+        pass
 
     def compose(self) -> ComposeResult:
-        for p in s.keys():
-            with Horizontal(classes='column'):
-                yield Static(p, id=f'set_{p}', classes='description')
-                yield MyInput(value=str(s[p]), id=f"val_{p}", classes='descr_value')
+        with VerticalScroll(id='settings'):
+            for p in self.KEYS.keys():
+                with Horizontal(classes='column'):
+                    yield Static(self.KEYS[p], id=f'set_{p}', classes='settings_label')
+                    yield MyInput(value=str(s[p]), id=f"val_{p}", classes='settings_value')
+        yield Footer()
+
+    async def on_key(self, event):
+        if event.key == 'down':
+            self.focus_next()
+        if event.key == 'up':
+            self.focus_previous()
+        await super()._on_key(event)
+
+    def action_ftr_save(self):
+        for p in self.KEYS.keys():
+            val = self.query_one(f"#val_{p}").value
+            try:
+                s[p] = int(val)
+            except ValueError:
+                try:
+                    s[p] = float(val)
+                except ValueError:
+                    s[p] = val
+        write_settings()
+        self.app.pop_screen()
+        self.post_message(self.Return())
+
+
+class Update(ModalScreen):
+    BINDINGS = [
+        Binding("escape", "app.pop_screen", "Pop screen"),
+    ]
+
+    t_click = time.time()
+
+    def __init__(self):
+        super().__init__()
+        self.path = pathlib.Path().resolve()
+
+    async def on_click(self, event):
+        if time.time() - self.t_click < 0.3:
+            self.log("Tree double click")
+        self.t_click = time.time()
+        await super()._on_click(event)
+
+    # def on_load(self):
+
+    def on_mount(self):
+        self.path = pathlib.Path().resolve()
+        self.query_one(DirectoryTree).path = self.path
+
+    def compose(self) -> ComposeResult:
+        yield DirectoryTree(self.path, id='update_tree')
+        with Horizontal():
+            yield Button("Parent", id='parent')
+            yield Button("Update", id='update')
+            yield Static("No file", id='upd_file')
+        yield ProgressBar()
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == 'parent':
+            tree = self.query_one(DirectoryTree)
+            tree.path = tree.path.parent
+        else:
+            pb = self.query_one(ProgressBar)
+            pb.update(total=100)
+
+    def on_directory_tree_file_selected(self, event: DirectoryTree.FileSelected):
+        st = self.query_one('#upd_file')
+        st.update(f"Selected file: {event.path}")
+
+    def on_directory_tree_directory_selected(self, event: DirectoryTree.DirectorySelected):
+        self.log(event.path)
 
 
 class LogApp(App):
     CSS_PATH = "TextMbAppCss.tcss"
     TITLE = "Modbus state"
-    SUB_TITLE = "Disconnected"
-    SCREENS = {"settings": Settings()}
+    # SUB_TITLE = "Disconnected"
+    SCREENS = {"settings": Settings(), "update":Update()}
 
     BINDINGS = [
-        Binding("c", "ftr_connect", "Connect"),
-        Binding("d", "ftr_disconnect", "Disconnect"),
         Binding("a", "ftr_read_all", "Read All"),
         Binding("r", "ftr_read", "Read"),
         Binding("s", "push_screen('settings')", "Settings"),
+        Binding("u", "push_screen('update')", "Update"),
         Binding("q", "ftr_quit", "Quit"),
     ]
 
     def __init__(self, visual='VisualSettings.json', upgrade='UpgradeSettings.json', com='ComSettings.json',
                  slave=None, reg_map=None, mb=None):
         super().__init__()
+        self._com_settings = com
         if isinstance(mb, MbClient):
             self.mb = mb
         else:
             self.mb = MbClient()
-        self._com_settings = com
+            self.mb.open(self._com_settings, connectionless=True)
+
         read_settings([com, visual, upgrade])
         self.slave = s['slave_address'] if slave is None else slave
         self.reg = RegMap(self.mb, slave=self.slave)
@@ -95,7 +185,7 @@ class LogApp(App):
         pass
 
     def on_mount(self):
-        self.log(self.tree)
+        self.update_sub_title()
         self._table = self.query_one(DataTable)
         self._table.cursor_type = "row"
         self._table.zebra_stripes = True
@@ -103,9 +193,6 @@ class LogApp(App):
         for reg in self.reg.input + self.reg.hold:
             self._table.add_row(f"{reg['Address']}", reg['Type'], reg['Name'], str(reg['Value']),
                                 self.reg.val_to_hex(reg))
-
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        self.exit(event.button.id)
 
     def on_data_table_row_highlighted(self, message):
         self._reg_idx = message.cursor_row
@@ -118,6 +205,13 @@ class LogApp(App):
 
     def on_my_input_return(self):
         self._table.focus()
+
+    def on_settings_return(self):
+        self.mb.open(self._com_settings, connectionless=True)
+        self.update_sub_title()
+
+    def update_sub_title(self):
+        self.sub_title = f"Using {self.mb.comport}, {self.mb.s['baud_rate']} baud/s, parity {self.mb.s['parity']}"
 
     @on(MyInput.Submitted, '#val_dec')
     def on_dec_submitted(self, message):
@@ -147,16 +241,6 @@ class LogApp(App):
     def action_ftr_quit(self):
         self.exit()
 
-    def action_ftr_connect(self):
-        self.mb.open(self._com_settings)
-        self.sub_title = "Connected"
-        self.log("Connected to ")
-
-    def action_ftr_disconnect(self):
-        self.mb.close()
-        self.sub_title = "Disconnected"
-        self.log("Disconnected from ")
-
     def _update_bottom(self, reg_name):
         st = self.query_one("#desc")
         in_dec = self.query_one("#val_dec")
@@ -178,8 +262,6 @@ class LogApp(App):
         self._table.update_cell_at((idx, 4), self.reg.val_to_hex(reg), update_width=True)
         if idx == self._reg_idx:
             self._update_bottom(reg['Name'])
-
-
 
 
 if __name__ == "__main__":
